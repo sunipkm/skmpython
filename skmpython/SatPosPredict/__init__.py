76171131b8daac16cb24af31f3e0599f7f0362fc
Sunip K. Mukherjee <sunipkmukherjee@gmail.com>
2023-05-01 16:19:40 -0400

    Updated TLE database stuff

--- skmpython/SatPosPredict/__init__.py
+++ skmpython/SatPosPredict/__init__.py
@@ -1,5 +1,6 @@
 from ._pospredict import ISSLatLonFromTstamp, ISSTleFromTstamp
+from ._build_tle_db import build_tledb_exec_fn
 
 LatLonFromTstamp = ISSLatLonFromTstamp
 
-__all__ = ['ISSLatLonFromTstamp', 'LatLonFromTstamp', 'ISSTleFromTstamp']
+__all__ = ['ISSLatLonFromTstamp', 'LatLonFromTstamp', 'ISSTleFromTstamp', 'build_tledb_exec_fn']
--- skmpython/SatPosPredict/_build_tle_db.py
+++ skmpython/SatPosPredict/_build_tle_db.py
@@ -1,5 +1,6 @@
 # %% Imports
 from __future__ import annotations
+import argparse
 from typing import Iterable, List, Tuple
 from tqdm import tqdm
 import xarray as xr
@@ -9,6 +10,7 @@ import sys
 from datetime import datetime, timedelta
 import pytz
 from dateutil.parser import parse
+import shutil
 
 import json
 import requests
@@ -45,6 +47,8 @@ def get_raw_tle_from_tstamp(ts: datetime | np.datetime64 | Iterable)->Tuple[date
             out.append(get_raw_tle_from_tstamp(t))
         out = list(filter(lambda x: x is not None, out))
         return np.asarray(out).T
+    else:
+        raise RuntimeError('ts must be datetime, np.datetime64, or Iterable of datetime or np.datetime64.')
 
     dayofweek = dict_dayofweek[ts.weekday()]
     day = ts.day
@@ -63,8 +67,9 @@ def get_raw_tle_from_tstamp(ts: datetime | np.datetime64 | Iterable)->Tuple[date
             return None
         tledict = json.loads(content.content)
         epoch = tledict['epoch']
+        epoch = int(parse(epoch + '+00:00').timestamp()*1e9)
         lines = tledict['jsTLE'].replace('\r', '').split('\n')
-        return (parse(epoch + '+00:00'), lines[0], lines[1], lines[2])
+        return (np.datetime64(epoch, 'ns'), lines[0], lines[1], lines[2]) # epoch is in UTC
     except requests.exceptions.RequestException as e:
         print(ts, e)
         return None
@@ -84,32 +89,60 @@ def get_tle_ds(tledb: np.ndarray):
     coords={'timestamp': tstamp}
     )
     return tle_ds.drop_duplicates(dim=..., keep='first')
-
 # %%
-start = pytz.utc.localize(datetime(2017, 5, 29))
-tstamps = []
-cond = True
-idx = 0
-while cond:
-    next = datetime.utcfromtimestamp(start.timestamp() + 15*60*idx)
-    idx += 1
-    tstamps.append(next)
-    if next > pytz.utc.localize(datetime(2017, 8, 1)).replace(tzinfo=None):
-        break
+def build_tledb_exec_fn():
+    parser = argparse.ArgumentParser(description='(Re)build TLE database for ISS.')
+    parser.add_argument('-s', '--start', type=str, help='start date in YYYYMMDD format', required=True)
+    parser.add_argument('-e', '--end', type=str, help='end date in YYYYMMDD format', required=True)
+    parser.add_argument('-d', '--db', type=str, help='database file name', default=None)
+    parser.usage = parser.format_help()
+    args = parser.parse_args()
+    start = parse(args.start).astimezone(pytz.utc)
+    end = parse(args.end).astimezone(pytz.utc)
+    if start < parse('1998-11-20').astimezone(pytz.utc):
+        raise ValueError('Start date must be after 1998-11-20 (launch of ISS)')
+    if end < start:
+        raise ValueError('End date must be after start date')
+    if end > datetime.now().astimezone(pytz.utc):
+        raise ValueError('End date must be before now')
+    if args.db is None:
+        db = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'ISS_TLE_DB.nc') # default, use the default Zarya storage
+    elif not os.path.isfile(): # use the specified database
+        raise OSError(f'{args.db} is not a file.')
+    elif not args.db.endswith('.nc'):
+        raise TypeError(f'Database file {args.db} must be a netCDF file.')
+    else:
+        db = args.db
+    
+    tstamps = []
+    cond = True
+    idx = 0
+    while cond:
+        next = datetime.utcfromtimestamp(start.timestamp() + 15*60*idx)
+        idx += 1
+        tstamps.append(next)
+        if next > end.replace(tzinfo=None):
+            break
+    out = get_raw_tle_from_tstamp(tstamps)
+    ds = get_tle_ds(out)
+    oname = f'ISS_TLE_DBPART_{datetime.now().strftime("%Y%d%m")}.nc'
+    if os.path.exists(oname): # allow for multiple runs in a day
+        count = 1
+        oname = oname.replace('.nc', f'_{count}.nc')
+        while os.path.exists(oname):
+            oname = oname.replace(f'_{count}.nc', f'_{count+1}.nc')
+            count += 1
+    ds.to_netcdf(oname) # save in calling directory
+    if os.path.exists(db):
+        ods = xr.load_dataset(db)
+        # back up current database to its location
+        shutil.copy(db, db.replace('.nc', f'_{datetime.now().strftime("%Y%d%m_%H%M%S")}.nc'))
+        nds = xr.concat([ds, ods], dim='timestamp')
+    else:
+        nds = ds
+    nds = nds.drop_duplicates(dim=..., keep='first')
+    nds = nds.sortby('timestamp')
+    nds.to_netcdf(db)
 # %%
-out = get_raw_tle_from_tstamp(tstamps)
-# %%
-ds = get_tle_ds(out)
-# %%
-ds.to_netcdf(f'ISS_TLE_DB_{datetime.now().strftime("%Y%d%m")}.nc')
-# %%
-if os.path.exists('ISS_TLE_DB.nc'):
-    ods = xr.load_dataset('ISS_TLE_DB.nc')
-    nds = xr.concat([ds, ods], dim='timestamp')
-else:
-    nds = ds
-nds = nds.drop_duplicates(dim=..., keep='first')
-nds = nds.sortby('timestamp')
-# %%
-nds.to_netcdf('ISS_TLE_DB.nc')
-# %%
+if __name__ == '__main__':
+    build_tledb_exec_fn()
